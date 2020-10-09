d71e34829514ef61567415dae4905349e73bf7ab
Violin <rizki_violin_radhiyan@git.com>
2020-10-09 07:23:16 +0700

    before merged

--- app/src/main/java/com/quick/plantapp/Adapter/PlantsAdapter.java
+++ app/src/main/java/com/quick/plantapp/Adapter/PlantsAdapter.java
@@ -5,6 +5,7 @@ import android.net.wifi.p2p.WifiP2pManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -12,7 +13,6 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.quick.plantapp.R;
 import com.quick.plantapp.Room.Plants;
-import com.quick.plantapp.ViewHolder.mPlantHolder;
 
 import java.util.List;
 
@@ -27,7 +27,7 @@ public class PlantsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_view, parent, false);
         return new mPlantHolder(v);
     }
@@ -42,4 +42,13 @@ public class PlantsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
     public int getItemCount() {
         return datalist.size();
     }
+
+    public class mPlantHolder extends RecyclerView.ViewHolder {
+        TextView nJudul, nDesc;
+
+        public mPlantHolder(@NonNull View itemView) {
+            super(itemView);
+            nJudul = itemView.findViewById(R.id.tv_judul);
+            nDesc =  itemView.findViewById(R.id.tv_deskripsi);
+        }
 }
